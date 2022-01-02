1a87947d1d8b11069444af2103cba42ff26a19f7
Przemek T <przemek8319@gmail.com>
2022-01-02 18:31:01 +0100

    formularz hasło + poprawność nagłówka

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class SingUpTest {
@@ -16,6 +19,7 @@ public class SingUpTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
 
     // Przycisk [My Account]
         driver.findElements(By.xpath("//li[@id='li_myaccount']"))
@@ -28,15 +32,30 @@ public class SingUpTest {
         driver.findElements(By.xpath("//a[text()='  Sign Up']"))
                 .get(1)
                 .click();
-
+        String lastName= "Nazwisko";
     // Formularz rejestracyjny
         driver.findElement(By.name("firstname"))
                 .sendKeys("Imię");
         driver.findElement(By.name("lastname"))
-                .sendKeys("Nazwisko");
+                .sendKeys(lastName);
         driver.findElement(By.name("phone"))
                 .sendKeys("123456789");
         driver.findElement(By.name("email"))
-                .sendKeys("ImieNazwisko@email.com");
+                .sendKeys("ImieNazwisko7@email.com");
+        driver.findElement(By.name("password"))
+                .sendKeys("haslo123");
+        driver.findElement(By.name("confirmpassword"))
+                .sendKeys("haslo123");
+        driver.findElements(By.xpath("//button[@type='submit']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+
+    // sprwadzenie nagłówka
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(),"Hi, Imię Nazwisko");
     }
 }
