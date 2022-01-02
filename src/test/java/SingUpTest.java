3e920290e60d25018a1bfec21bd8c88546f35931
Przemek T <przemek8319@gmail.com>
2022-01-02 13:50:40 +0100

    Przycisk My Account/ Sing Up

---
+++ src/test/java/SingUpTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+
+public class SingUpTest {
+
+    @Test
+    public void singUp() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+    // Przycisk [My Account]
+        driver.findElements(By.xpath("//li[@id='li_myaccount']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+
+    // Przycisk [Sing Up]     -mniej polecana metoda
+        driver.findElements(By.xpath("//a[text()='  Sign Up']"))
+                .get(1)
+                .click();
+    }
+}
