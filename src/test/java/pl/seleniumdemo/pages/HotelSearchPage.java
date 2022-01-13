f477b5be7ef939202cab43cd33ebc7f911b578c7
Przemek T <przemek8319@gmail.com>
2022-01-13 19:55:46 +0100

    patern page object

---
+++ src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
@@ -0,0 +1,55 @@
+package pl.seleniumdemo.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HotelSearchPage {
+
+    @FindBy(className = "select2-chosen")
+    private WebElement searchHotel;
+
+    @FindBy(xpath = "//div[@id='select2-drop']//input")
+    private WebElement searchHotelInput;
+
+    @FindBy(xpath = "//span[@class='select2-match']")
+    private WebElement hotelMatch;
+
+    @FindBy(name = "checkin")
+    private WebElement checkinInput;
+
+    @FindBy(name = "checkout")
+    private WebElement checkoutInput;
+
+    @FindBy(id = "travellersInput")
+    private WebElement travellersInput;
+
+    @FindBy(id = "adultPlusBtn")
+    private WebElement adultPlusBtn;
+
+    @FindBy(id = "childPlusBtn")
+    private WebElement childPlusBtn;
+
+    @FindBy(xpath = "//button[@type='submit' and text()=' Search']")
+    private WebElement searchButton;
+
+    public void setCity(String cityName){
+        searchHotel.click();
+        searchHotelInput.sendKeys("Dubai");
+        hotelMatch.click();
+    }
+
+    public void setDates(String checkin, String checkout){
+        checkinInput.sendKeys("21/01/2021");
+        checkoutInput.sendKeys("234/01/2021");
+    }
+
+    public void setTravellers(){
+        travellersInput.click();
+        adultPlusBtn.click();
+        childPlusBtn.click();
+    }
+
+    public void performSearch(){
+        searchButton.click();
+    }
+}
