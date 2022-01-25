e0c2fe38036b1d1f4e2430bd78f987cbe6190ebe
Przemek T <przemek8319@gmail.com>
2022-01-25 09:05:32 +0100

    wykonywanie testów na różnych przeglądarkach

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -3,8 +3,10 @@ package pl.seleniumdemo.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.seleniumdemo.utils.DriverFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,8 +16,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = DriverFactory.getDriver("firefox");
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
