a9c9384920e26bf8142617e3dae964e118607abc
Przemek T <przemek8319@gmail.com>
2022-03-17 18:13:58 +0100

    automatyczny screenshot do testu

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.pages.ResultsPage;
 import pl.seleniumdemo.utils.ExcelReader;
+import pl.seleniumdemo.utils.SeleniumHelper;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,18 +18,19 @@ public class HotelSearchTest extends BaseTest {
     protected static ExtentTest test;
 
     @Test
-    public void searchHotelTest() {
+    public void searchHotelTest() throws IOException {
 
         test = extentReports.createTest("Search Hotel Test");
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.setCity("Dubai");
-        test.log(Status.PASS, "Setting city done");
+        test.log(Status.PASS, "Setting city done", SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setDates("21/01/2022", "24/02/2023");
-        test.log(Status.PASS, "Setting dates done");
+        test.log(Status.PASS, "Setting dates done", SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setTravellers(1, 2);
-        test.log(Status.PASS, "Setting traveller done");
+        test.log(Status.PASS, "Setting traveller done", SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.performSearch();
         test.log(Status.PASS, "Performing search done");
+        test.log(Status.PASS, "Screenshot", SeleniumHelper.getScreenshot(driver));
 
         ResultsPage resultsPage = new ResultsPage(driver);
         List<String> hotelNames = resultsPage.getHotelNames();
@@ -37,49 +39,49 @@ public class HotelSearchTest extends BaseTest {
         Assert.assertEquals(hotelNames.get(1), "Oasis Beach Tower");
         Assert.assertEquals(hotelNames.get(2), "Rose Rayhaan Rotana");
         Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
-        test.log(Status.PASS, "Assertions passed");
+        test.log(Status.PASS, "Assertions passed", SeleniumHelper.getScreenshot(driver));
     }
 
     @Test
-    public void searchHotelWithoutNameTest() {
-        test = extentReports.createTest("Search Hotel Without Name Test");
+    public void searchHotelWithoutNameTest() throws IOException {
+        test = extentReports.createTest("Search Hotel Test Without City");
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         test.log(Status.PASS, "City name skip");
         hotelSearchPage.setDates("21/02/2021", "25/02/2021");
-        test.log(Status.PASS, "Setting dates done");
+        test.log(Status.PASS, "Setting dates done",SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setTravellers(0, 1);
-        test.log(Status.PASS, "Setting traveller done");
+        test.log(Status.PASS, "Setting traveller done",SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.performSearch();
-        test.log(Status.PASS, "Performing search done");
+        test.log(Status.PASS, "Performing search done",SeleniumHelper.getScreenshot(driver));
 
         ResultsPage resultsPage = new ResultsPage(driver);
 
         Assert.assertTrue(resultsPage.resultHeading.isDisplayed());
         Assert.assertEquals(resultsPage.getHeadingText(), "No Results Found");
-        test.log(Status.PASS, "Assertions passed");
+        test.log(Status.PASS, "Assertion done",SeleniumHelper.getScreenshot(driver));
     }
     @Test (dataProvider = "data")
-    public void searchHotelTestWithDataProvider(String city, String hotel) {
+    public void searchHotelTestWithDataProvider(String city, String hotel) throws IOException {
 
-        test = extentReports.createTest("Search Hotel Test With Data Provider");
+        test = extentReports.createTest("Search Hotel Test With Data Provider for" + city);
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.setCity(city);
-        test.log(Status.PASS, "Setting city done");
+        test.log(Status.PASS, "Setting city done",SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setDates("21/01/2022", "24/02/2023");
-        test.log(Status.PASS, "Setting dates done");
+        test.log(Status.PASS, "Setting dates done",SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setTravellers(1, 2);
-        test.log(Status.PASS, "Setting traveller done");
+        test.log(Status.PASS, "Setting traveller done",SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.performSearch();
-        test.log(Status.PASS, "Performing search done");
+        test.log(Status.PASS, "Performing search done",SeleniumHelper.getScreenshot(driver));
 
         ResultsPage resultsPage = new ResultsPage(driver);
         List<String> hotelNames = resultsPage.getHotelNames();
 
         Assert.assertEquals(hotelNames.get(0), hotel);
-        test.log(Status.PASS, "Assertions passed");
+        test.log(Status.PASS, "Assertion done",SeleniumHelper.getScreenshot(driver));
     }
     @DataProvider
     public Object[][] data() throws IOException {
-        return ExcelReader.readExcel("testData.xls");
+        return ExcelReader.readExcel("testData.xlsx");
     }
 }
--- src/test/java/pl/seleniumdemo/utils/SeleniumHelper.java
+++ src/test/java/pl/seleniumdemo/utils/SeleniumHelper.java
@@ -1,10 +1,14 @@
 package pl.seleniumdemo.utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SeleniumHelper {
 
@@ -21,5 +25,19 @@ public class SeleniumHelper {
     public static void waitForNotEmptyList(WebDriver driver, By locator){
         WebDriverWait wait = new WebDriverWait(driver, 10l);
         wait.until(browser-> browser.findElements(locator).size()>0);
+    }
+
+    public static MediaEntityModelProvider getScreenshot(WebDriver driver) throws IOException {
+        String path = takeScreenshot(driver);
+        return MediaEntityBuilder.createScreenCaptureFromPath(path).build();
+    }
+
+    private static String takeScreenshot (WebDriver driver) throws IOException {
+        int randomNumber = (int) (Math.random()*1000);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        String path = "src/test/resources/screenshots/screenshot" + randomNumber + ".png";
+        FileUtils.copyFile(file, new File(path));
+        return path;
     }
 }
